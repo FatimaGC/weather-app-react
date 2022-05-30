87d79559332e0cd77a4aa2ff19a09ea23b143a27
Fatima Guzman <fguzman1230@gmail.com>
2022-05-30 13:43:54 -0600

    Added DateTimeDisplay component to SiteHeader

---
+++ src/components/DateTimeDisplay/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function DateTimeDisplay() {
+  return (
+    <div>
+      <p>
+        <time datetime="2022-04-12T14:54:39.929Z" id="date">
+          May 30th
+        </time>
+      </p>
+    </div>
+  );
+}
