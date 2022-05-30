8f8b373e6ff802dafd87be54fa9bf5f7829746ca
Fatima Guzman <fguzman1230@gmail.com>
2022-05-30 13:46:09 -0600

    Added SearchBar component

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchBar({ onSubmit }) {
+  const [searchValue, setSearchValue] = useState("");
+  function handleChange(event) {
+    setSearchValue(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmit(searchValue);
+  }
+  return (
+    <form
+      action=""
+      id="search-form"
+      aria-label="search form"
+      onSubmit={handleSubmit}
+    >
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">
+          Enter a City
+        </span>
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+          id="search-text-input"
+          value={searchValue}
+          onChange={handleChange}
+        />
+        <button
+          class="btn btn-outline-secondary"
+          type="submit"
+          id="button-addon2"
+          value="Search"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
