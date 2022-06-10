2bdfdbe368f643063d44526aad5459f3c03f9238
Fatima Guzman <fguzman1230@gmail.com>
2022-06-10 11:42:24 -0600

    Updated units in Degrees components to only show Fahrenheit

--- src/components/CurrentWeather/components/Degrees/index.js
+++ src/components/CurrentWeather/components/Degrees/index.js
@@ -7,15 +7,7 @@ export default function Degrees({ temperature, description }) {
       <p className="current-weather_degrees" id="current-weather">
         {temperature}°
       </p>
-      <span className="current-weather_units">
-        <a href="/" id="celsius-link">
-          C
-        </a>
-        |
-        <a href="/" id="fahrenheit-link">
-          F
-        </a>
-      </span>
+      <span className="current-weather_units">F</span>
       <span className="current-weather_description">{description}</span>
     </div>
   );
