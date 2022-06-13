0fef2022dc94fdee03275fd28e998245c8812a0a
Fatima Guzman <fguzman1230@gmail.com>
2022-06-13 10:02:47 -0600

    Added code to display WeeklyForecast data

---
+++ src/components/WeeklyForecast/DailyForecast/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "../../Image";
+
+function day(dailyWeatherEntry) {
+  if (dailyWeatherEntry?.dt) {
+    let date = new Date(dailyWeatherEntry.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+    return days[day];
+  } else {
+    return "";
+  }
+}
+
+export default function DailyForecast({ dailyWeatherEntry }) {
+  const weekDay = day(dailyWeatherEntry);
+  const icon = dailyWeatherEntry?.weather[0].icon;
+  const iconDescription = dailyWeatherEntry?.weather[0].icon;
+  const maxTemperature = Math.round(dailyWeatherEntry?.temp.max);
+  const minTemperature = Math.round(dailyWeatherEntry?.temp.min);
+
+  return (
+    <div>
+      <div className="weekly-forecast_container row" id="forecast">
+        <div className="col-2">
+          <h3>{weekDay}</h3>
+          <span className="weather-icon">
+            <Image icon={icon} iconDescription={iconDescription} />
+          </span>
+          <p>
+            {maxTemperature}° |{" "}
+            <span className="weekly-forecast_min_degrees">
+              {" "}
+              {minTemperature}°
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/WeeklyForecast/index.js
+++ src/components/WeeklyForecast/index.js
@@ -1,57 +1,36 @@
 import React, { useState, useEffect } from "react";
-import Image from "../Image";
+import DailyForecast from "./DailyForecast";
 import axios from "axios";
 
 export default function WeeklyForecast({ coordinates }) {
-  const [weekDay, setWeekDay] = useState("");
-  const [icon, setIcon] = useState("");
-  const [iconDescription, setIconDescription] = useState("");
-  const [maxTemperature, setMaxTemperature] = useState("");
-  const [minTemperature, setMinTemperature] = useState("");
+  const [dailyWeatherData, setdailyWeatherData] = useState([]);
+  // const [hasError, setHasError] = useState(false);
 
   const apiKey = "0a4dc3c696be7291e8d469a7dbee552f";
 
   useEffect(() => {
-    if (coordinates) {
+    if (coordinates.latitude && coordinates.longitude) {
       const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${apiKey}&units=imperial`;
       axios.get(apiUrl).then(handleSuccess);
 
       function handleSuccess(response) {
-        console.log(response);
-        function day() {
-          let date = new Date(response.data.daily[0].dt * 1000);
-          let day = date.getDay();
-          let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
-          return days[day];
-        }
-        setWeekDay(day);
-        setIcon(response.data.daily[0].weather[0].icon);
-        setIconDescription(response.data.daily[0].weather[0].icon);
-        setMaxTemperature(Math.round(response.data.daily[0].temp.max));
-        setMinTemperature(Math.round(response.data.daily[0].temp.min));
+        setdailyWeatherData(response.data.daily);
+        console.log(response.data.daily);
       }
     }
   }, [coordinates]);
 
   return (
-    <div>
+    <>
       <h2 className="weekly-forecast_title">Weekly Forecast</h2>
 
-      <div className="weekly-forecast_container" id="forecast">
-        <div className="col">
-          <h3>{weekDay}</h3>
-          <span className="weather-icon">
-            <Image icon={icon} iconDescription={iconDescription} />
-          </span>
-          <p>
-            {maxTemperature}° |{" "}
-            <span className="weekly-forecast_min_degrees">
-              {" "}
-              {minTemperature}°
-            </span>
-          </p>
-        </div>
-      </div>
-    </div>
+      <ul>
+        {dailyWeatherData.map((dailyWeatherEntry) => (
+          <li key={dailyWeatherEntry.dt}>
+            <DailyForecast dailyWeatherEntry={dailyWeatherEntry} />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
